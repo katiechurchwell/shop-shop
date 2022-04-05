6d064849d58d9e98604b258c0d32a2cb052442d5
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-04-05 11:53:54 -0700

    StoreProvider component to manage global state

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+    //instantiate global state with useProductReducer()
+    //state: most up-to-date version of global state object
+    //dispatch: method to update state
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: '',
+    });
+
+    console.log(state);
+    //return state object and dispatch through Provider component
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+}
+
+export { StoreProvider, useStoreContext };
